a26795a0e03ae1845594ed2480bd2c62e4b976ef
Kenton Li <kentonli622@gmail.com>
2022-09-21 12:21:38 +0800

    Test CI unit testing

--- app/src/test/java/gradle/test/AppTest.java
+++ app/src/test/java/gradle/test/AppTest.java
@@ -11,4 +11,8 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void thisWillAlwaysFail() {
+        fail("This test is always failed");
+    }
 }
